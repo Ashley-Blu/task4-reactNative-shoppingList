e8e041f2a62b4ab70ea2fa598c1c2d437db22c8c
Ashley-Blu <matsekolengashley28@hmail.com>
2026-01-13 14:52:41 +0200

    Added a navigation button to the shopping list screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,36 @@
-import { Text, View } from "react-native";
+import { Link } from "expo-router";
+import { Text, View, StyleSheet, Pressable } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Pressable style={styles.button}>
+        <Text style={styles.text}>
+          <Link href={"/list"}>Get started</Link>
+        </Text>
+      </Pressable>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  button: {
+    backgroundColor: "black",
+    padding: 12,
+    paddingHorizontal: 130,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 6,
+  },
+
+  text: {
+    color: "white",
+    fontWeight: 'bold'
+  },
+});
