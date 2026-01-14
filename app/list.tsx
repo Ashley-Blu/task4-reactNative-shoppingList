36e7dbe652bdbfbb602f44e0cce3abd7030a0824
Ashley-Blu <matsekolengashley28@hmail.com>
2026-01-14 12:02:13 +0200

    created add button that opens a modal

--- app/list.tsx
+++ app/list.tsx
@@ -1,8 +1,51 @@
-import { Text, View, StyleSheet } from "react-native";
+import AddButton from "@/components/AddButton";
+import { useState } from "react";
+import { Image, Modal, StyleSheet, Text } from "react-native";
+import AddItemScreen from "./AddItemScreen";
 
-export default function ShoppingListScreen() {
-  return;
-  <View>
-    <Text>Shopping List</Text>
-  </View>;
-}
+const ShoppingListScreen = () => {
+  const [modalVissible, setModalVissible] = useState(false);
+
+  return (
+    <>
+      <Image
+        style={styles.basketImg}
+        source={require("../assets/images/basket.png")}
+      />
+      <Image
+        style={styles.trollyImg}
+        source={require("../assets/images/trolly.png")}
+      />
+      <Text style={styles.text}>
+        Oops! There is nothing on your list. Click the plus button at the bottom
+        to add to your list.
+      </Text>
+
+      <AddButton onPress={() => setModalVissible(true)} />
+      <Modal visible={modalVissible} animationType="slide">
+        <AddItemScreen onPress={() => setModalVissible(false)} />
+      </Modal>
+    </>
+  );
+};
+
+export default ShoppingListScreen;
+
+const styles = StyleSheet.create({
+  basketImg: {
+    width: 130,
+    height: 130,
+    marginHorizontal: "auto",
+  },
+
+  trollyImg: {
+    width: 230,
+    height: 230,
+    opacity: 1,
+    marginHorizontal: "auto",
+  },
+
+  text: {
+    textAlign: "center",
+  },
+});
